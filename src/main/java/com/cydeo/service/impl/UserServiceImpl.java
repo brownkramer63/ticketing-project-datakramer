e28a4249490be0903f2ed328735d86519bc925c1
Kramer Brown <brownkramer63@gmail.com>
2022-11-03 10:58:40 -0500

    usermapping

--- src/main/java/com/cydeo/service/impl/UserServiceImpl.java
+++ src/main/java/com/cydeo/service/impl/UserServiceImpl.java
@@ -1,15 +1,31 @@
 package com.cydeo.service.impl;
 
 import com.cydeo.dto.UserDTO;
+import com.cydeo.entity.User;
+import com.cydeo.repository.UserRepository;
 import com.cydeo.service.UserService;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class UserServiceImpl implements UserService {
+
+   private final UserRepository userRepository;
+
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
     @Override
     public List<UserDTO> listAllUsers() {
+        //we just need find all by the sort by will make it fancy
+
+        List<User> userList = userRepository.findAll(Sort.by("firstName"));
+
+
+
         return null;
     }
 
