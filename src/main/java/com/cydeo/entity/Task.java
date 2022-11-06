372e58a604b87af49b4c012d0b37a3de3f205470
Kramer Brown <brownkramer63@gmail.com>
2022-11-06 10:31:49 -0600

    projectpart

---
+++ src/main/java/com/cydeo/entity/Task.java
@@ -0,0 +1,35 @@
+package com.cydeo.entity;
+
+import com.cydeo.enums.Status;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.Where;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "Task")
+@Where(clause = "is_deleted=false")
+public class Task extends BaseEntity{
+
+    private String taskSubject;
+    private String taskDetail;
+    @Enumerated(EnumType.STRING)
+    private Status taskStatus;
+    @Column(columnDefinition = "DATE")
+    private LocalDate assignedDate;
+    @ManyToOne(fetch = FetchType.LAZY)
+    private User assignedEmployee;
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Project project;
+
+
+
+}
